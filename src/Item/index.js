d939f87e574914bf89a4fbf3734e566800f46d8e
liuxicui <870566031@qq.com>
2016-12-04 09:28:59 +0800

    blog add

--- src/Item/index.js
+++ src/Item/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import marked from 'marked';
-
+import axios from 'axios';
+import Loading from '../component/Loading';
 
 class Item extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      data:''
+    }
+  }
+  componentDidMount(){
+    let address = this.props.params.title;
+    axios.get(`https://raw.githubusercontent.com/liuxicui/11-28-demo/master/data/${address}.md`)
+      .then(res => this.setState({data:res.data}) )
+      .catch( err => alert(err))
+  }
   render () {
-    let content = this.props.params.title == 0 ? '这是第一个页面' :
-                  this.props.params.title == 1 ? '这是第二个页面' :
-                  this.props.params.title == 2 ? '这是第三个页面' : '这是第n个页面'
     return(
-      <div>
-        {content}
-        {marked("8")}
-        <div dangerouslySetInnerHTML={{__html:marked('8')}}/>
+      <div className='item-wrap'>
+        {
+          this.state.data.length==0 ? <Loading /> :
+              <div dangerouslySetInnerHTML={{__html:marked(this.state.data)}}/>
+        }
       </div>
     )
   }
